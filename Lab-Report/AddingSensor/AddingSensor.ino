cd744d7e3af19898f763d5ac34277de49a6ec325
Thomas Ferrer <thomas.ferrer@hei.yncrea.fr>
2019-10-11 10:44:28 +0200

    ajout addingSensor code + screenShot

---
+++ Lab-Report/AddingSensor/AddingSensor.ino
@@ -0,0 +1,1 @@
+/**/void setup() {  Serial.begin(9600);}void loop() {  int sensorValue = analogRead(A0);  Serial.print("light: ");  Serial.println(sensorValue);  float percentage = map(sensorValue, 0, 1023, 0, 100);  Serial.print("light percentage: ");  Serial.print(percentage);  Serial.println(" % ");  delay(1000);}
